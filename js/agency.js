39e202bd42e826942fbc0f6a52ce5b3d3294a79d
Wilson Qin <wilson.qin@gmail.com>
2015-04-24 16:25:21 -0400

    removed extraneous sections and re-classing

--- js/agency.js
+++ js/agency.js
@@ -6,13 +6,13 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    // $('a.page-scroll').bind('click', function(event) {
+    //     var $anchor = $(this);
+    //     $('html, body').stop().animate({
+    //         scrollTop: $($anchor.attr('href')).offset().top
+    //     }, 1500, 'easeInOutExpo');
+    //     event.preventDefault();
+    // });
 });
 
 // Highlight the top nav as scrolling occurs
